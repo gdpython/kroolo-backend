de40fad4087f1ca6cec29885fcccbda1ecb2c4f4
prasahnt-kroolo <prashant.sharma@kroolo.com>
2023-10-14 19:50:21 +0530

    model replce location

--- model/mongoose/index.js
+++ model/mongoose/index.js
@@ -6,8 +6,6 @@
 const User = require("./authentication/User");
 const Department = require("./authentication/Department");
 const Country = require("./authentication/Country");
-const Organizations = require("./authentication/Organizations");
-const OrganizationMember = require("./authentication/OrganizationMember");
 const Role = require("./authentication/Role");
 const Route = require("./authentication/Route");
 const RouteRole = require("./authentication/RouteRole");
@@ -19,13 +17,21 @@ const UserToken = require("./authentication/UserToken");
  * @property {mongoose.Model<User>} User - Mongoose model for the User entity.
  * @property {mongoose.Model<Department>} Department - Mongoose model for the Department entity.
  * @property {mongoose.Model<Country>} Country - Mongoose model for the Country entity.
- * @property {mongoose.Model<Organizations>} Organizations - Mongoose model for the Organizations entity.
- * @property {mongoose.Model<OrganizationMember>} OrganizationMember - Mongoose model for the OrganizationMember entity.
  * @property {mongoose.Model<Role>} Role - Mongoose model for the Role entity.
  * @property {mongoose.Model<Route>} Route - Mongoose model for the Route entity.
  * @property {mongoose.Model<RouteRole>} RouteRole - Mongoose model for the RouteRole entity.
  * @property {mongoose.Model<UserToken>} UserToken - Mongoose model for the UserToken entity.
  */
+/**
+ * A module that exports all Mongoose models for authentication and related entities.
+ *
+ * @typedef {object} MongooseModels
+ * @property {mongoose.Model<Organizations>} Organizations - Mongoose model for the Organizations entity.
+ * @property {mongoose.Model<OrganizationMember>} OrganizationMember - Mongoose model for the OrganizationMember entity.
+ */
+
+const Organizations = require("./organization/Organizations");
+const OrganizationMember = require("./organization/OrganizationMember");
 
 const Workspace = require("./workspace/Workspace");
 const WorkspaceMember = require("./workspace/WorkspaceMember");
